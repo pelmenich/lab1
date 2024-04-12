da7947b69fe00acadefd1627d457f7fbe1e1aeca
tsalyona <olenatsarevaaa@gmail.com>
2024-04-12 23:00:56 +0300

    lab done

---
+++ Lab1_C#/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Parallel_computing_processes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Enter the number of threads: ");
+            int numThreads = int.Parse(Console.ReadLine());
+
+            new Program().StartPoint(numThreads);
+        }
+
+        private void StartPoint(int numThreads)
+        {
+            Thread[] calculatorThreads = new Thread[numThreads];
+            Thread[] delayTime = new Thread[numThreads];
+
+            for (int i = 0; i < numThreads; i++)
+            {
+                Console.Write($"Enter the time (sec) of delay for thread: ");
+                int timeDelay = int.Parse(Console.ReadLine());
+
+                Breaker breaker = new(timeDelay);
+                delayTime[i] = new Thread(() => breaker.SetDelay());
+                calculatorThreads[i] = new Thread(() => Sum(breaker));
+            }
+
+            for (int i = 0; i < numThreads; i++)
+            {
+                calculatorThreads[i].Start();
+                delayTime[i].Start();
+            }
+        }
+
+        private void Sum(object obj)
+        {
+            Breaker breaker = (Breaker)obj;
+            long sum = 0;
+            while (!breaker.Stop)
+            {
+                sum++;
+            }
+            Console.WriteLine($"Id: {Environment.CurrentManagedThreadId} -  result: {sum}");
+        }
+    }
+}
+
+public class Breaker
+{
+    public bool Stop { get; private set; } = false;
+
+    private int _delayTime;
+
+    public Breaker(int delayTime)
+    {
+        _delayTime = delayTime;
+    }
+
+    public void SetDelay()
+    {
+        Thread.Sleep(_delayTime * 1000);
+        Stop = true;
+    }
+}
